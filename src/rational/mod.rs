b1ddcd8231933f878299fa70c7683062dee84997
Vamato <62358492+VamatoHD@users.noreply.github.com>
2026-01-22 20:20:39 +0000

    Improved ratio macro

--- src/rational/mod.rs
+++ src/rational/mod.rs
@@ -6,27 +6,40 @@ use unsigned::Unsigned;
 
 use core::num::NonZeroU128;
 
-macro_rules! Ratio {
-    //Handling zero cases
-    (0) => {{ Rational::zero() }};
-    (-0) => {{ Rational::zero() }};
-    (0 / $b:expr) => {{ Rational::zero() }};
-    (- 0 / $b:expr) => {{ Rational::zero() }};
-    ($a:literal / 0) => {{ compile_error!("Division by zero.") }};
-    //Handling just the numerator cases
-    (-$a:literal) => {{ -Ratio!($a) }};
-    ($a:literal) => {{ Ratio!($a / 1) }};
+macro_rules! rat {
+    // Zero cases
+    (0 $(/ $_:literal)?) => {{ Rational::zero() }};
+    (-0 $(/ $_:literal)?) => {{ Rational::zero() }};
 
-    (-$a:literal / $b:literal) => {{ -Ratio!($a / $b) }};
+    //Division by zero
+    ($_:literal / 0) => {{ compile_error!("division by zero.") }};
+    ($_:literal / -0) => {{ compile_error!("division by zero.") }};
+
+    //Just the numerator
+    (-$a:literal) => {{ -rat!($a / 1) }};
+    ($a:literal) => {{ rat!($a / 1) }};
+
+    //Main case
+    (-$a:literal / $b:literal) => {{ -rat!($a / $b) }};
     ($a:literal / $b:literal) => {{
-        match Rational::new($a as u128, $b as u128, false) {
+        //Todo: if NUM or DEN are bigger than i128
+        const NUM: i128 = $a;
+        const DEN: i128 = $b;
+        const SIGN: bool = NUM.is_negative() ^ DEN.is_negative();
+        const ABS_NUM: u128 = NUM.unsigned_abs();
+        const ABS_DEN: u128 = DEN.unsigned_abs();
+
+        match Rational::new(ABS_NUM, ABS_DEN, SIGN) {
             Some(res) => res,
             //Safety: den is non-zero
             None => unreachable!(),
         }
     }};
 
-    () => {{ compile_error!("Invalid Ratio") }};
+    //Invalid case
+    ($($tt:tt)*) => {
+        compile_error!(concat!("invalid rational literal: ", stringify!($($tt)*)))
+    };
 }
 
 macro_rules! to_nonzeroU128 {
@@ -183,24 +196,23 @@ impl PartialEq for Rational {
 #[cfg(test)]
 mod tests {
     use super::Rational;
-    const MAX: u128 = !0;
 
     #[test]
     fn eq_test() {
         assert_eq!(Rational::zero(), Rational::zero());
-        assert_eq!(Ratio!(2 / 3), Ratio!(4 / 6));
-        assert_eq!(Ratio!(0), Ratio!(0));
-        assert_ne!(Ratio!(-2 / 3), Ratio!(2 / 3));
-        assert_ne!(Ratio!(-4 / 3), Ratio!(8 / 6));
-        assert_ne!(Ratio!(-0 / 100), Ratio!(8 / 2));
-        assert_ne!(Ratio!(0 / MAX), Ratio!(8 / 2));
-        assert_eq!(Ratio!(0 / MAX), Ratio!(-0));
+        assert_eq!(rat!(2 / 3), rat!(4 / 6));
+        assert_eq!(rat!(0), rat!(0));
+        assert_ne!(rat!(-2 / 3), rat!(2 / 3));
+        assert_ne!(rat!(-4 / 3), rat!(8 / 6));
+        assert_ne!(rat!(-0 / 100), rat!(8 / 2));
+        assert_ne!(rat!(0 / 1000), rat!(8 / 2));
+        assert_eq!(rat!(0 / 1000), rat!(-0));
     }
 
     #[test]
     fn neg_test() {
-        assert_eq!(Ratio!(0), -Ratio!(0));
-        assert_eq!(Ratio!(-1), -Ratio!(1));
+        assert_eq!(rat!(0), -rat!(0));
+        assert_eq!(rat!(-1), -rat!(1));
         assert_eq!(
             Rational::from_nonzero(to_nonzeroU128!(1), to_nonzeroU128!(1), true),
             -Rational::from_nonzero(to_nonzeroU128!(1), to_nonzeroU128!(1), false)
@@ -214,31 +226,38 @@ mod tests {
 
     #[test]
     fn add_test() {
-        assert_eq!(Ratio!(1) + Ratio!(1), Ratio!(2));
-        assert_eq!(Ratio!(1 / 2) + Ratio!(1 / 2), Ratio!(1));
-        assert_eq!(Ratio!(-1 / 2) + Ratio!(1 / 2), Ratio!(0));
-        assert_eq!(Ratio!(99 / 100) + Ratio!(1 / 100), Ratio!(1));
-        assert_eq!(Ratio!(3 / 5) + Ratio!(7 / 11), Ratio!(68 / 55));
-        assert_eq!(Ratio!(3 / 5) + Ratio!(-7 / 11), Ratio!(-2 / 55));
-        assert_eq!(Ratio!(-7 / 11) + Ratio!(3 / 5), Ratio!(-2 / 55));
-        assert_eq!(Ratio!(-2) + Ratio!(-2), Ratio!(-4));
+        assert_eq!(rat!(1) + rat!(1), rat!(2));
+        assert_eq!(rat!(1 / 2) + rat!(1 / 2), rat!(1));
+        assert_eq!(rat!(-1 / 2) + rat!(1 / 2), rat!(0));
+        assert_eq!(rat!(99 / 100) + rat!(1 / 100), rat!(1));
+        assert_eq!(rat!(3 / 5) + rat!(7 / 11), rat!(68 / 55));
+        assert_eq!(rat!(3 / 5) + rat!(-7 / 11), rat!(-2 / 55));
+        assert_eq!(rat!(-7 / 11) + rat!(3 / 5), rat!(-2 / 55));
+        assert_eq!(rat!(-2) + rat!(-2), rat!(-4));
     }
 
     #[test]
     fn reduce_test() {
         //Rational::new automatically reduces
-        assert_eq!(Ratio!(0).reduce(), Ratio!(0));
-        assert_eq!(Ratio!(2 / 4), Ratio!(1 / 2));
-        assert_eq!(Ratio!(2 / 4), Ratio!(1 / 2));
-        assert_eq!(Ratio!(100 / 50), Ratio!(8 / 4));
-        assert_eq!(Ratio!(-100 / 50), -Ratio!(8 / 4));
+        assert_eq!(rat!(0).reduce(), rat!(0));
+        assert_eq!(rat!(2 / 4), rat!(1 / 2));
+        assert_eq!(rat!(2 / 4), rat!(1 / 2));
+        assert_eq!(rat!(100 / 50), rat!(8 / 4));
+        assert_eq!(rat!(-100 / 50), -rat!(8 / 4));
     }
 
     #[test]
     fn mul_test() {
-        assert_eq!(Ratio!(0) * Ratio!(0), Ratio!(0));
-        assert_eq!(Ratio!(2) * Ratio!(4), Ratio!(8));
-        assert_eq!(Ratio!(-2) * Ratio!(1 / 2), Ratio!(-1));
-        assert_eq!(Ratio!(-1) * Ratio!(-1), Ratio!(1));
+        assert_eq!(rat!(0) * rat!(0), rat!(0));
+        assert_eq!(rat!(2) * rat!(4), rat!(8));
+        assert_eq!(rat!(-2) * rat!(1 / 2), rat!(-1));
+        assert_eq!(rat!(-1) * rat!(-1), rat!(1));
     }
+}
+
+fn mul_test() {
+    assert_eq!(rat!(0) * rat!(0), rat!(0));
+    assert_eq!(rat!(2) * rat!(4), rat!(8));
+    assert_eq!(rat!(-2) * rat!(1 / 2), rat!(-1));
+    assert_eq!(rat!(-1) * rat!(-1), rat!(1));
 }
