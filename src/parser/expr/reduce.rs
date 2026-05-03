a64f855ba7e3dcb78f0d8d8c818df30e4f52cfca
Vamato <62358492+VamatoHD@users.noreply.github.com>
2026-05-03 11:31:40 +0100

    Expr: Logarithm identities
    Signed-off-by: Vamato <62358492+VamatoHD@users.noreply.github.com>

--- src/parser/expr/reduce.rs
+++ src/parser/expr/reduce.rs
@@ -1,5 +1,5 @@
 use super::Expr;
-use crate::{Ctx, Error};
+use crate::{Ctx, Error, Rational, lexer::Op};
 
 impl Expr {
     pub fn reduce(&self, ctx: &Ctx) -> Result<Expr, Error> {
@@ -36,6 +36,32 @@ impl Expr {
                 func_obj.apply_args(&reduced_args).reduce(ctx)?
             }
 
+            // Logarithms
+            Expr::Log { base, arg } => match arg.reduce(ctx)? {
+                Expr::Infix { lhs, op, rhs } if matches!(op, Op::Mul | Op::Div) => {
+                    // Both lhs and rhs should be reduced, as arg is reduced
+                    let new_op = if op == Op::Mul { Op::Add } else { Op::Sub };
+                    Expr::Infix {
+                        lhs: Box::new(Expr::Log {
+                            base: base.clone(),
+                            arg: lhs.clone(),
+                        }),
+                        op: new_op,
+                        rhs: Box::new(Expr::Log {
+                            base: base.clone(),
+                            arg: rhs.clone(),
+                        }),
+                    }
+                    .reduce(ctx)?
+                }
+                Expr::Number(n) if n == 1u128 => Expr::Number(Rational::zero()),
+                reduced => Expr::Log {
+                    base: base.clone(),
+                    arg: Box::new(reduced),
+                },
+            },
+
+            // No reduce options
             v => v.clone(),
         })
     }
